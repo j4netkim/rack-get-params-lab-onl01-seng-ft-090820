d6d57aa388ec051d94ec97a28e6a5a7b3b967a21
J K <jkim65@buffalo.edu>
2020-10-20 01:12:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,14 @@
 class Application
 
-  @@cart = ["Apples","Carrots","Pears"]
+  @@items = ["Apples","Carrots","Pears"]
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/cart/)
-      @@items.each do |cart|
-        resp.write "#{cart}\n"
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
@@ -21,7 +21,7 @@ class Application
   end
 
   def handle_search(search_term)
-    if @@cart.include?(search_term)
+    if @@items.include?(search_term)
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
